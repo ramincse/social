01b9462886247005a3bd6dc6b5b74fbdefd8c9a9
ramincse <ramin.cse2009@gmail.com>
2021-01-02 18:49:45 +0600

    Social project setup

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Development Area</title>
+		<!-- ALL CSS FILES  -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/responsive.css">
+	</head>
+	<body>
+		<div class="wrap shadow">
+			<div class="card">
+				<div class="card-body">
+					<h2>Login Now</h2>
+					<form action="">
+						<div class="form-group">
+							<label for="">Username / Email</label>
+							<input class="form-control" type="text">
+						</div>						
+						<div class="form-group">
+							<label for="">Password</label>
+							<input class="form-control" type="password">
+						</div>						
+						<div class="form-group">
+							<input class="btn btn-primary" type="submit" value="Log In">
+						</div>
+					</form>
+					<a class="btn btn-sm btn-info" href="register.php">Create an account</a>
+				</div>
+			</div>
+		</div>		
+		<!-- JS FILES  -->
+		<script src="assets/js/jquery-3.4.1.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/custom.js"></script>
+	</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Development Area</title>
+		<!-- ALL CSS FILES  -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/responsive.css">
+	</head>
+	<body>
+		<div class="wrap shadow">
+			<div class="card clearfix">
+				<div class="card-header">Profile of - Ruhul Amin <a class="btn btn-sm btn-info float-right" href="data.php">All members</a></div>
+				<div class="card-body">
+					<img style="width: 200px; height: 200px; display: block; margin: auto;" src="assets/media/img/pp_photo/82e2ae94.webp" alt="">
+
+					<table class="table table-striped">
+						<tr>
+							<td>Name</td>
+							<td>Ruhul Amin</td>
+						</tr>
+						<tr>
+							<td>Username</td>
+							<td>Ruhul Amin</td>
+						</tr>
+						<tr>
+							<td>Email</td>
+							<td>Ruhul Amin</td>
+						</tr>
+					</table>
+				</div>
+				<div class="card-footer">
+					<a class="btn btn-sm btn-info" href="logout.php">Logout</a>
+				</div>
+			</div>
+		</div>		
+		<!-- JS FILES  -->
+		<script src="assets/js/jquery-3.4.1.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/custom.js"></script>
+	</body>
+</html>
